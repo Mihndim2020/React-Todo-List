8ecc4b37437b9f08c93a4cc4a8afc0016e5f949c
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-02-25 13:01:35 -0500

    Add useState hook to manage the name variable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+function addTask(name) {
+  alert(name);
+}
+
 function App(props) {
 
   const taskList = props.tasks.map(task => (
@@ -11,7 +15,7 @@ function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
