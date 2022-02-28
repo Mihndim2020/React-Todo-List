3b7c2e035dfbe2f8c7495102a4d1ce58a03ca5d7
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-02-28 14:34:42 -0500

    Add unique IDs to tasks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-function addTask(name) {
-  alert(name);
-}
 
 function App(props) {
   
   const [tasks, setTasks] = useState(props.tasks);
 
+  function addTask(name) {
+    const newTask = { id: "todo" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
   const taskList = tasks.map(task => (
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
   ));
   
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -25,7 +31,7 @@ function App(props) {
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list" className="todo-list stack-large stack-exception"
