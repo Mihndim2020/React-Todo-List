7f4907e6786201a389b86dd2387c8f6cd80f1c6b
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-03-01 13:51:51 -0500

    Update function to toggle the state of a todo

--- src/App.js
+++ src/App.js
@@ -15,8 +15,18 @@ function App(props) {
   }
 
   function toggleTaskCompleted(id) {
-    console.log(tasks[0])
-  }  
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
 
   const taskList = tasks.map(task => (
     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} />
