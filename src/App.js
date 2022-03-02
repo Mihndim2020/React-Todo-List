8533fd90e003ab7af890220a9a7d66742481b849
Mihndim2020 <jelsieprisca2016@gmail.com>
2022-03-02 14:59:28 -0500

    Add view and editing templates

--- src/App.js
+++ src/App.js
@@ -32,11 +32,22 @@ function App(props) {
     setTasks(remainingTasks);
   }
   
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+    // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
   
   
 
   const taskList = tasks.map(task => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} />
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask}  editTask={editTask} />
   ));
   
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
